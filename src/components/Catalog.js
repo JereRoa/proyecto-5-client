eb85d6f6aa4d562f7ae89d4ca5c1c32e30f8825f
JereRoa <jereroa.digital@gmail.com>
2023-11-21 18:14:28 -0300

    primer commit client

---
+++ src/components/Catalog.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ListProducts from './ListProduct'
+
+export default function Catalog() {
+    return (
+        <div>
+            <ListProducts title="Todos los productos gaming" />
+        </div>
+    )
+}
