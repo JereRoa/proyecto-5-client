91673f348120a541252631e445a796d0ff4a9023
JereRoa <jereroa.digital@gmail.com>
2023-11-30 20:21:29 -0300

    prueba

--- src/components/ListProduct/index.js
+++ src/components/ListProduct/index.js
@@ -58,6 +58,7 @@ console.log('aquí van las props', props)
               return (
 
                 <Link to={`/${e._id}`} key={e._id}>
+                  
                   <div className="group relative">
                     <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
                       <img src={ e.imagen} alt="Guitar" className="w-full h-full object-center object-cover lg:w-full lg:h-full" />
